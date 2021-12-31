666c13c5007a5a1c2b4cc195919a0f60ebe45f89
urosmiha <mihajlovic.uros.96@gmail.com>
2021-12-31 16:43:33 +1300

    add colour highlight function
    highlight only specified colour... make everything else black

--- main.py
+++ main.py
@@ -10,7 +10,35 @@ import cv2
 #     cv2.imshow('frame', img)
 
 
+def highlightColour(frame):
+
+    # covert image to hue saturation brightness image
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # light blue pixels
+    lower_blue = np.array([90, 50, 50])
+    # darker blue pixels
+    upper_blue = np.array([130, 255, 255])
+
+    # create a mask so we can run bitwise-and on it to filer out the colour
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+    # only keep pixels that match the mask pixel displayed
+    result = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow('hsv', result)
+
+
+def convertBGRtoHSV(BGR_value):
+    # BGR_colour = np.array([[BGR_value]])
+    BGR_colour = np.array([[[255, 0, 0]]])
+    x = cv2.cvtColor(BGR_colour, cv2.COLOR_BGR2HSV)
+    print("HSV Value:")
+    print(x)
+
+
 def main():
+    print("read camera...")
     # Read camera
     cap = cv2.VideoCapture(0)
 
@@ -22,22 +50,10 @@ def main():
         width = int(cap.get(3))
         height = int(cap.get(4))
 
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        # light blue
-        lower_blue = np.array([90, 50, 50])
-        # darker blue
-        upper_blue = np.array([130, 255, 255])
+        highlightColour(frame)
 
-        mask = cv2.inRange(hsv, lower_blue, upper_blue)
-
-        # only keep pixels that match the mask pixel displayed
-        result = cv2.bitwise_and(frame, frame, mask=mask)
-
-        cv2.imshow('hsv', result)
-
-        # image = np.zeros(frame.shape, np.uint8)
         # Display image
-        # cv2.imshow('frame', image)
+        # cv2.imshow('frame', result)
 
         if cv2.waitKey(1) == ord('q'):
             break
@@ -47,4 +63,5 @@ def main():
 
 
 if __name__ == "__main__":
+    # convertBGRtoHSV([255,0,0])
     main()
